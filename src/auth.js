9e5beebfeed8b736d24458c4ef38bf7348b4e99d
Facundo <facundobazzi@gmail.com>
2024-05-05 23:59:46 -0300

    fixeo detalles para serverless offline

--- src/auth.js
+++ src/auth.js
@@ -4,7 +4,13 @@ module.exports.handler = async(event) => {
     { name: "admin", token: "secretAdminToken" }
   ]
   
-  const user = users.find(x => x.token === event.headers.token)
+  const token = event.headers.authorization 
+    ? event.headers.authorization.split(' ')[1]
+    : event.headers.Authorization
+      ? event.headers.Authorization.split(' ')[1]
+      : ''
+
+  const user = users.find(x => x.token === token)
 
   return {
     "isAuthorized": user !== undefined,
